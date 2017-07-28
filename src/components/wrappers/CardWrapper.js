b57aa2dec07b250571df94021d1fc9cfaf300a26
ArtixZ <artixzhu@gmail.com>
2017-07-27 23:57:30 -0400

    layouting card

--- src/components/wrappers/CardWrapper.js
+++ src/components/wrappers/CardWrapper.js
@@ -1,42 +1,67 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { Card, ListItem, Button } from 'react-native-elements'
+import { Rating } from 'react-native-elements';
 
 import { CardHorizontal, CardSection } from '../common';
-import { Card, ListItem, Button } from 'react-native-elements'
 
 
 const CardWrapper = ({ thumbnail_image, foodName, tags, suggestion, rating, price, highlights, distance }) => {
 
-    const { thumbnailStyle, thumbnailContainerStyle, contentStyle } = styles;
+    const { thumbnailStyle, thumbnailContainerStyle, contentContainerStyle, ratingSty, txtCardContainer } = styles;
 
     return (
-        <Card flexDirection={'row'}>
-            <Card style={{flex:1}}>
+        <Card containerStyle={{ flex: 1}} wrapperStyle={{ flex: 1, flexDirection: 'column' }}>
+            <View flex={4} flexDirection={'row'}>
                 <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
-                    />
+                        />
                 </View>
-                <Text>{suggestion}</Text>
-            </Card>
-            <Card style={{flex:1}}>
-                <View style={contentStyle}>
+                <View style={contentContainerStyle}>
                     <Text>{foodName}</Text>
-                    <Text>{rating}</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around' }}>
+                        <Rating
+                            imageSize={20}
+                            readonly
+                            startingValue={rating}
+                            style={ratingSty}
+                            />
+                        <Text>{`${distance} mi`}</Text>
+                    </View>
                     <Text>{highlights.toString()}</Text>
-                    <Text>{`${distance} mi`}</Text>
+                </View>
+            </View>
+            <View flex={1} flexDirection={'row'} alignItems={'center'}>
+                <View style={txtCardContainer}>
+                    <Text>{suggestion}</Text>
+                </View>
+                <View style={txtCardContainer}>
                     <Text>{`$${price}`}</Text>
-                </View>     
-            </Card>
+                </View>
+            </View>
         </Card>
     );
 };
 
 const styles = {
-    contentStyle: {
+    ratingSty: {
+
+    },
+    txtCardContainer: {
+        backgroundColor: '#5b7',
+        flex: 1,
+        height: '100%',
+        justifyContent: 'center',
+    },
+    bkgColor: {
+        backgroundColor: '#bfa',
+    },
+    contentContainerStyle: {
+        flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center'
+        justifyContent: 'space-around'
     },
     thumbnailStyle: {
         height: 100,
