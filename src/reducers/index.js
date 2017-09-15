deb858e66950081c81eb57eb8bdb0f8ca1d81976
ArtixZ <artixzhu@gmail.com>
2017-09-14 23:18:12 -0400

    Add Camera reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
-import EmployeeFormReducer from './EmployeeFormReducer';
-import EmployeeReducer from './EmployeeReducer';
 import ChatMessageReducer from './ChatMessageReducer';
+import CameraReducer from './CameraReducer';
 
 export default combineReducers({
   auth: AuthReducer,
-  employeeForm: EmployeeFormReducer,
-  employees: EmployeeReducer,
   messages: ChatMessageReducer,
+  cameraImg: CameraReducer
 });
