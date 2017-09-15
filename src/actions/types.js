daba32a2bad06c9a888f9b5f5da4c27fa67d232c
ArtixZ <artixzhu@gmail.com>
2017-09-14 23:27:30 -0400

    add camera related types

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,10 +4,13 @@ export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
 
-export const EMPLOYEE_UPDATE = 'employee_update';
-export const EMPLOYEE_CREATE = 'employee_create';
-export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
-export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
+/*--------------- chat ---------------------*/
 
 export const TXT_CHAT_MESSAGE = 'TXT_CHAT_MESSAGE';
 export const TXT_RESPONSE_MESSAGE = 'TXT_RESPONSE_MESSAGE';
+export const FOOD_CLASS = 'FOOD_CLASS';
+
+/*--------------- camera ---------------------*/
+
+export const CAMERA_IMAGE_URI = 'CAMERA_IMAGE_URI';
+export const CAMERA_IMAGE_FOOD_IMG = 'CAMERA_IMAGE_FOOD_IMG';
