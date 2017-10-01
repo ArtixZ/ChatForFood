16990e7c9a14467565db94da85a21e5b61ca5f24
ArtixZ <artixzhu@gmail.com>
2017-10-01 13:58:30 -0400

    change shadow opacity for cardwrapper

--- src/components/wrappers/CardWrapper.js
+++ src/components/wrappers/CardWrapper.js
@@ -67,7 +67,7 @@ const styles = {
                 borderBottomWidth: 0,
                 shadowColor: '#000',
                 shadowOffset: { width: 0, height: 2 },
-                shadowOpacity: 0.8,
+                shadowOpacity: 0.1,
                 shadowRadius: 2,
                 elevation: 1,
                 marginTop: 10,
